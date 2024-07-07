03002a2324fc52f10c74d67106717022e7bb4c94
JameGran <tamef.granados@gmail.com>
2024-07-07 05:08:41 -0500

    added skills section

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MisRedesComponent } from "../../components/mis-redes/mis-redes.component";
+import { SkillsComponent } from "../../components/skills/skills.component";
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+    selector: 'app-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css',
+    imports: [MisRedesComponent, SkillsComponent]
 })
 export class AboutComponent {
 
